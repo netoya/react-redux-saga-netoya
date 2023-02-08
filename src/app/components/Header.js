f95330387f799ffea33d2708f40e093a94bfa690
Angel Oliver <angel.oliver.cv@gmail.com>
2023-02-08 18:32:47 -0300

    [readme] info

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function Header() {
-  return (
-    <div className={"h-10 bg-gray-400"}>
-      <Link to={"/home"}>HOme</Link>
-      <Link to={"/contacto"}>Contacto</Link>
-    </div>
-  );
+    return (
+        <div className={"h-10 bg-gray-400"}>
+            <Link to={"/"}>HOme</Link>
+            <Link to={"/contacto"}>Contacto</Link>
+        </div>
+    );
 }
 
 export default Header;
